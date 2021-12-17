fd3ff9fa3836c51a387ce54983f531fc1fbf281a
Marcel Eichler <marcel.eichler@outlook.com>
2021-12-17 17:30:29 +0100

    restructured code

--- src/main/java/de/marcel1802/tsbot/core/init.java
+++ src/main/java/de/marcel1802/tsbot/core/init.java
@@ -22,102 +22,14 @@ public class init {
     public static void main(String[] args) {
 
         settingsAsClass settingsObj = loadSettings();
+
         if (settingsObj == null) {
-            // todo
             System.out.println("Config error");
         }
         else {
-
-            final TS3Config config = new TS3Config();
-            config.setHost(settingsObj.getGeneral_ip());
-            config.setEnableCommunicationsLogging(true);
-
-            final TS3Query query = new TS3Query(config);
-            query.connect();
-
-            final TS3Api api = query.getApi();
-            api.login(settingsObj.getGeneral_loginname(), settingsObj.getGeneral_loginpassword());
-            api.selectVirtualServerById(settingsObj.getGeneral_virtualServerID());
-
-            try {
-                api.setNickname(settingsObj.getGeneral_displayname());
-            } catch (Exception ex) {
-                //todo
-            }
-
-
-            api.moveQuery(settingsObj.getGeneral_joinChannel());
-
-            Set<Integer> moveDefault_list = new HashSet<>();
-
-            api.registerEvent(TS3EventType.SERVER);
-            api.addTS3Listeners(new TS3EventAdapter() {
-
-                @Override
-                public void onClientJoin(ClientJoinEvent e) {
-                    if (settingsObj.isMoveDefault_enabled()) {
-                        moveDefault_list.add(e.getClientId());
-                        new Thread(() -> {
-                            try {
-                                Thread.sleep(1000L * settingsObj.getMoveDefault_moveTime());
-                                if (moveDefault_list.contains(e.getClientId()) && api.getClientInfo(e.getClientId()).getChannelId() == settingsObj.getMoveDefault_defaultChannelID()) {
-                                    api.moveClient(e.getClientId(), settingsObj.getMoveDefault_AFKChannelID());
-                                    if (settingsObj.getMoveDefault_mode() == 1) {
-                                        api.sendPrivateMessage(e.getClientId(), settingsObj.getMoveDefault_message());
-                                    } else if (settingsObj.getMoveDefault_mode() == 2) {
-                                        api.pokeClient(e.getClientId(), settingsObj.getMoveDefault_message());
-                                    }
-                                    moveDefault_list.remove(e.getClientId());
-                                }
-                            } catch (Exception ex) {
-                                // todo
-                            }
-                        }).start();
-                    }
-
-                    if (settingsObj.isNotifyComplaints_enabled()) {
-                        if (!api.getComplaints().isEmpty()) {
-                            List<String> servergroups = Arrays.asList(e.getClientServerGroups().split("\\s*,\\s*"));
-                            if (servergroups.contains(Integer.toString(settingsObj.getNotifyComplaints_adminGroup()))) {
-                                if (settingsObj.getNotifyComplaints_mode() == 1) {
-                                    api.sendPrivateMessage(e.getClientId(), settingsObj.getNotifyComplaints_message());
-                                } else if (settingsObj.getNotifyComplaints_mode() == 2) {
-                                    api.pokeClient(e.getClientId(), settingsObj.getNotifyComplaints_message());
-                                }
-                            }
-                        }
-                    }
-
-                    if (settingsObj.isDefaultNicknameCheck_enabled()) {
-                        if (e.getClientNickname().equalsIgnoreCase("teamspeakuser")) {
-                            if (settingsObj.getDefaultNicknameCheck_mode() == 1) {
-                                api.sendPrivateMessage(e.getClientId(), settingsObj.getDefaultNicknameCheck_message());
-                            }
-                            else if (settingsObj.getDefaultNicknameCheck_mode() == 2) {
-                                api.pokeClient(e.getClientId(), settingsObj.getDefaultNicknameCheck_message());
-                            }
-                            else {
-                                api.kickClientFromServer(settingsObj.getDefaultNicknameCheck_message(), e.getClientId());
-                            }
-                        }
-                    }
-                }
-
-                @Override
-                public void onClientLeave(ClientLeaveEvent e) {
-                    if (settingsObj.isMoveDefault_enabled()) {//&& moveDefault_list.contains(e.getClientId())
-                        moveDefault_list.remove(e.getClientId());
-                    }
-                }
-
-                @Override
-                public void onClientMoved(ClientMovedEvent e) {
-                    if (settingsObj.isMoveDefault_enabled()) {//&& moveDefault_list.contains(e.getClientId())
-                            moveDefault_list.remove(e.getClientId());
-                    }
-                }
-            });
+            startTeamspeakBot(settingsObj);
         }
+
     }
 
     public static settingsAsClass loadSettings() {
@@ -129,5 +41,99 @@ public class init {
         catch(Exception ex) {
             return null;
         }
+    }
+
+    public static void startTeamspeakBot(settingsAsClass loadedSettings) {
+
+        final TS3Config config = new TS3Config();
+        config.setHost(loadedSettings.getGeneral_ip());
+        config.setEnableCommunicationsLogging(true);
+
+        final TS3Query query = new TS3Query(config);
+        query.connect();
+
+        final TS3Api api = query.getApi();
+        api.login(loadedSettings.getGeneral_loginname(), loadedSettings.getGeneral_loginpassword());
+        api.selectVirtualServerById(loadedSettings.getGeneral_virtualServerID());
+
+        try {
+            api.setNickname(loadedSettings.getGeneral_displayname());
+        } catch (Exception ex) {
+            //todo
+        }
+
+
+        api.moveQuery(loadedSettings.getGeneral_joinChannel());
+
+        Set<Integer> moveDefault_list = new HashSet<>();
+
+        api.registerEvent(TS3EventType.SERVER);
+        api.addTS3Listeners(new TS3EventAdapter() {
+
+            @Override
+            public void onClientJoin(ClientJoinEvent e) {
+                if (loadedSettings.isMoveDefault_enabled()) {
+                    moveDefault_list.add(e.getClientId());
+                    new Thread(() -> {
+                        try {
+                            Thread.sleep(1000L * loadedSettings.getMoveDefault_moveTime());
+                            if (moveDefault_list.contains(e.getClientId()) && api.getClientInfo(e.getClientId()).getChannelId() == loadedSettings.getMoveDefault_defaultChannelID()) {
+                                api.moveClient(e.getClientId(), loadedSettings.getMoveDefault_AFKChannelID());
+                                if (loadedSettings.getMoveDefault_mode() == 1) {
+                                    api.sendPrivateMessage(e.getClientId(), loadedSettings.getMoveDefault_message());
+                                } else if (loadedSettings.getMoveDefault_mode() == 2) {
+                                    api.pokeClient(e.getClientId(), loadedSettings.getMoveDefault_message());
+                                }
+                                moveDefault_list.remove(e.getClientId());
+                            }
+                        } catch (Exception ex) {
+                            // todo
+                        }
+                    }).start();
+                }
+
+                if (loadedSettings.isNotifyComplaints_enabled()) {
+                    if (!api.getComplaints().isEmpty()) {
+                        List<String> servergroups = Arrays.asList(e.getClientServerGroups().split("\\s*,\\s*"));
+                        if (servergroups.contains(Integer.toString(loadedSettings.getNotifyComplaints_adminGroup()))) {
+                            if (loadedSettings.getNotifyComplaints_mode() == 1) {
+                                api.sendPrivateMessage(e.getClientId(), loadedSettings.getNotifyComplaints_message());
+                            } else if (loadedSettings.getNotifyComplaints_mode() == 2) {
+                                api.pokeClient(e.getClientId(), loadedSettings.getNotifyComplaints_message());
+                            }
+                        }
+                    }
+                }
+
+                if (loadedSettings.isDefaultNicknameCheck_enabled()) {
+                    if (e.getClientNickname().equalsIgnoreCase("teamspeakuser")) {
+                        if (loadedSettings.getDefaultNicknameCheck_mode() == 1) {
+                            api.sendPrivateMessage(e.getClientId(), loadedSettings.getDefaultNicknameCheck_message());
+                        }
+                        else if (loadedSettings.getDefaultNicknameCheck_mode() == 2) {
+                            api.pokeClient(e.getClientId(), loadedSettings.getDefaultNicknameCheck_message());
+                        }
+                        else {
+                            api.kickClientFromServer(loadedSettings.getDefaultNicknameCheck_message(), e.getClientId());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onClientLeave(ClientLeaveEvent e) {
+                if (loadedSettings.isMoveDefault_enabled()) {//&& moveDefault_list.contains(e.getClientId())
+                    moveDefault_list.remove(e.getClientId());
+                }
+            }
+
+            @Override
+            public void onClientMoved(ClientMovedEvent e) {
+                if (loadedSettings.isMoveDefault_enabled()) {//&& moveDefault_list.contains(e.getClientId())
+                    moveDefault_list.remove(e.getClientId());
+                }
+            }
+        });
+
     }
 }
